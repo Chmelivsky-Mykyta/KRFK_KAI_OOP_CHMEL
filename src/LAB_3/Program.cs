08cd8d46d645cddd106daf2e0e2ae7b0b94bb4be
Микита <nikita.chmelivsky@krfk.kai.edu.ua>
2025-06-02 09:23:40 +0300

    added lab3,4

---
+++ src/LAB_3/Program.cs
@@ -0,0 +1,46 @@
+﻿
+namespace LAB3
+{
+    public class program
+    {
+        public static void Main(string[] args)
+        {
+            TaskOne();
+            TaskTwo();
+            TaskThree();
+        }
+        //1
+        private static void TaskOne()
+
+        {
+            int userAge = 20;
+            string userName = "Андрій";
+
+            Console.WriteLine("Привіт, " + userName + "! Твій вік: " + userAge);
+        }
+
+        //2
+        private static void TaskTwo()
+        {
+            int number = 10;
+            string message = "Hi";
+            bool isNew = true;
+
+            Console.WriteLine(number + " " + message + " " + isNew);
+        }
+
+        //3
+        private static void TaskThree()
+        {
+            // Оголошуємо змінну name та присвоюємо їй значення "Анна"
+            string name = "Анна";
+            // Оголошуємо змінну age та присвоюємо їй значення 25
+            int age = 25;
+            // Виводимо на консоль привітання з ім'ям і віком користувача
+            Console.WriteLine("Привіт, " + name + "! Твій вік: " + age);
+        }
+
+    }
+}
+
+
---
+++ src/LAB_4/Program.cs
@@ -0,0 +1,35 @@
+﻿//1
+int age = 18;
+double weight = 56.7;
+char grade = 'A'; ;
+bool isStudent = true;
+string name = "Микита";
+Console.WriteLine($"Вік: {age}\nВага: {weight}\nОцінка: {grade}\nСтудент: {isStudent}\nІм'я: {name}");
+//2 
+Console.Write("Введіть число: ");
+double x = Convert.ToDouble(Console.ReadLine());
+int y = (int)x;
+string z = y.ToString();
+Console.WriteLine($"Double: {x}\nInt: {y}\nString: {z}");
+//3 
+Console.Write("Введіть ваше ім'я: ");
+string Name = Console.ReadLine();
+Console.Write("Введіть ваш вік: ");
+int Age = int.Parse(Console.ReadLine());
+Console.WriteLine($"Привіт, {Name}! Твій вік: {Age} років.");
+//4 V — середня швидкість (км/год) S — відстань (км) t — час (год)
+Console.Write("Введіть відстань (км): ");
+double s = double.Parse(Console.ReadLine());
+Console.Write("Введіть час (год): ");
+double t = double.Parse(Console.ReadLine());
+
+double v = s / t;
+Console.WriteLine($"Середня швидкість: {v} км/год");
+*/
+//5
+Console.Write("Введіть речення: ");
+string text = Console.ReadLine();
+Console.WriteLine($"Довжина: {text.Length} символів");
+Console.WriteLine($"Верхній регістр: {text.ToUpper()}");
+Console.WriteLine($"Замінені пробіли: {text.Replace(" ", "-")}");
+
